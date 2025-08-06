492013d7ec81577ea4f946e2cfd5262636de9c76
2framesper <xloliver222@gmail.com>
2025-08-05 22:18:35 -0400

    Complete js exercise 10: leap years
    Create 3 consts for divisible by 4, 100, and 400
    Use an if statement checking whether divisible by 4 and either not a century or divisible by 400
    if true, return true, else, false

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByCentury = year % 100 === 0;
+    const isDivisibleByFourCenturies = year % 400 === 0;
+    if (isDivisibleByFour && (!isDivisibleByCentury || isDivisibleByFourCenturies)){
+        return true
+    } else {
+        return false
+    }
+}
 // Do not edit below this line
 module.exports = leapYears;
