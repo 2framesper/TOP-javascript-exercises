fa98f3da609efe2aeac43bb5fc90ac672ab42e2f
2framesper <xloliver222@gmail.com>
2025-08-05 20:58:59 -0400

    Complete js exercise 06: repeat string
    use a for loop to repeat the string a certain number of times, if num is a negative number, return 'ERROR'

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = '';
+    for (i=0; i<num; i++) {
+        newString += string;
+    }
 
+    if (num < 0) {
+        return "ERROR";
+    }
+    return newString
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
