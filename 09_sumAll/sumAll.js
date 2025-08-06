513e823eb23bc86bc93ed49a11aa584249500cfd
2framesper <xloliver222@gmail.com>
2025-08-05 21:55:55 -0400

    Complete js exercise 09: sum all
    Take in 2 parameters: max and min (reverse if placed in a different order), only work if they are positive integers, return 'ERROR' if not
    Use a for loop to iterate adding onto the min by one each time, add up all the numbers from min to max, both included

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return 'ERROR';
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
 
+    if (min > max){
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    let total = 0;
+
+    for (i=min; i <= max; i++){
+        total += i;
+    }
+
+
+    return total
 };
 
 // Do not edit below this line
