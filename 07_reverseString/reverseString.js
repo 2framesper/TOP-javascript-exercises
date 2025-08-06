b419b4c866da3f40f623efcf9097080b3ebc4421
2framesper <xloliver222@gmail.com>
2025-08-05 21:16:21 -0400

    Complete js exercise 07: reverse string
    Create a function called reverseString that reverses the string given
    use .split to turn it into an array to manipulate, then .join it back into a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    arr = str.split('');
+    let newArr=[];
+    for (let i = 0; i < arr.length; i++) {
+        newArr[i] = arr[arr.length-i-1]
+    }
+    return str = newArr.join('')
 };
 
 // Do not edit below this line
